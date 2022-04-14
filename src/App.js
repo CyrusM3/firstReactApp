dfcbf2b50f4967578b7c4eda86d8d26ed1d6a809
Ali Ebrahimi <74895984+CyrusM3@users.noreply.github.com>
2022-04-13 18:33:45 -0700

    index.css changed bk:red

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>Hello Teammate. We are Un4gettable! Rememeber this...</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
