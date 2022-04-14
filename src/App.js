a17f267c8533151d8d1f73777e96fcc57de506e9
Ali Ebrahimi <74895984+CyrusM3@users.noreply.github.com>
2022-04-13 20:05:29 -0700

    mofified h1, h2,h3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Team mates. We are Un4gettable! Remember this.</h1>
+      <h1>Hello Team mates.</h1>
+      <h2>We are Un4gettable!</h2>
+      <h3>Remember this.</h3>
     </div>
   );
 }
