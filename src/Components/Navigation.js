1283ad53bb061524cd288b473d044b2e970bc9dd
Ali Ebrahimi <74895984+CyrusM3@users.noreply.github.com>
2022-04-17 00:23:40 -0700

    made humberger menu, added responsive menu

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,13 +1,15 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
-// import hero from "../images/img1.jpg";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navigation = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="navbar">
       <div className="hero"></div>
-      <ul className="nav-menu">
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li className="nav-item">
           <a href="#">Home</a>
         </li>
@@ -21,6 +23,13 @@ const Navigation = () => {
           <a href="#">Contact</a>
         </li>
       </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={30} style={{ color: "#f8f8f8" }} />
+        ) : (
+          <FaBars size={30} style={{ color: "#f8f8f8" }} />
+        )}
+      </div>
       <div>
         <h1>
           Let's <span className="text">Explore</span> The World
